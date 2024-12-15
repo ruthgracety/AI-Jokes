a3dac534394e89ca609b5f6c343818e4fa679582
ruthgracety <ruthgracety@gmail.com>
2024-12-15 20:16:17 +0000

    added API integration

--- src/index.js
+++ src/index.js
@@ -1,13 +1,25 @@
-function generateJokes(event) {
-    event.preventDefault();
-
+function displayJokes(response) {
     new Typewriter("#jokes", {
-      strings: "List of Jokes",
+      strings: response.data.answer,
       autoStart: true,
       delay: 1,
       cursor: "",
     });
 }
 
+function generateJokes(event) {
+    event.preventDefault();
+
+let instructionsInput = document.querySelector("#user-instructions");
+let apiKey = "ccc4b1tc7of0e402eb0f24b35faed2d9";
+let context = "You are a well known comedian with the best authentic jokes ever";
+let prompt = `Tell me 3 funniest jokes about ${instructionsInput.value} and list each joke in numbering form on each line with a <br /> without saying numbered as requested`;
+let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+axios.get(apiURL).then(displayJokes);
+    
+}
+
 let jokesFormElement = document.querySelector("#jokes-generate-form");
 jokesFormElement.addEventListener("submit", generateJokes);
+
