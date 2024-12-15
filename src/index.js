359a8c959265b771edc17dcceb301b6116c4cdf0
ruthgracety <ruthgracety@gmail.com>
2024-12-15 19:34:49 +0000

    detailing the js and added typewriter plugin

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
-alert("hey")
+function generateJokes(event) {
+    event.preventDefault();
+
+    new Typewriter("#jokes", {
+      strings: "List of Jokes",
+      autoStart: true,
+      delay: 1,
+      cursor: "",
+    });
+}
+
+let jokesFormElement = document.querySelector("#jokes-generate-form");
+jokesFormElement.addEventListener("submit", generateJokes);
